1317afdbb3ad4d2205cd7bea34e5a4ac22bee936
Franjo Đido <franjo.djido@outlook.com>
2020-03-03 00:43:25 +0100

    Add files via upload

---
+++ admin/includes/header.php
@@ -0,0 +1,13 @@
+<div class="brand clearfix">
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+			
+			<li class="ts-account">
+				<a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> Admin <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="change-password.php">Promijeni lozinku</a></li>
+					<li><a href="logout.php">Odjava</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
---
+++ admin/includes/leftbar.php
@@ -0,0 +1,25 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Navigacija</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Upravljačka ploča</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Brendovi automobila</a>
+<ul>
+<li><a href="create-brand.php">Dodaj</a></li>
+<li><a href="manage-brands.php">Uredi</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Automobili</a>
+					<ul>
+						<li><a href="post-avehical.php">Dodaj novi</a></li>
+						<li><a href="manage-vehicles.php">Uredi</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Rezervacije</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Lista korisnika</a></li>
+				<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Kontakt informacije</a></li>
+
+			</ul>
+		</nav>
